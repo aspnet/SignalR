eeed633575dccf9b4cdcf00f1f8679ee8c0ec4a6
moozzyk <pawelka@microsoft.com>
2016-12-21 14:36:00 -0800

    Fixing send/poll race
    Fixing a bug where the server would throw if send request is received before the first poll or sse request (#57). We will now delay send until the first poll or sse is received or reject the send request if poll or sse not received within a timeout
    Also making the connection not to be marked as Active upon reserving a connection. This enables cleaning up connections that were reserved but never used.

--- test/Microsoft.AspNetCore.SignalR.Client.FunctionalTests/HubConnectionTests.cs
+++ test/Microsoft.AspNetCore.SignalR.Client.FunctionalTests/HubConnectionTests.cs
@@ -2,7 +2,11 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
+using System.IO;
 using System.IO.Pipelines;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -10,6 +14,7 @@ using Microsoft.AspNetCore.Sockets.Client;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Moq;
 using Xunit;
 
 namespace Microsoft.AspNetCore.SignalR.Client.FunctionalTests
@@ -46,8 +51,6 @@ namespace Microsoft.AspNetCore.SignalR.Client.FunctionalTests
                 var transport = new LongPollingTransport(httpClient, loggerFactory);
                 using (var connection = await HubConnection.ConnectAsync(new Uri("http://test/hubs"), new JsonNetInvocationAdapter(), transport, httpClient, pipelineFactory, loggerFactory))
                 {
-                    //TODO: Get rid of this. This is to prevent "No channel" failures due to sends occuring before the first poll.
-                    await Task.Delay(500);
                     var result = await connection.Invoke<string>("HelloWorld");
 
                     Assert.Equal("Hello World!", result);
@@ -67,8 +70,6 @@ namespace Microsoft.AspNetCore.SignalR.Client.FunctionalTests
                 var transport = new LongPollingTransport(httpClient, loggerFactory);
                 using (var connection = await HubConnection.ConnectAsync(new Uri("http://test/hubs"), new JsonNetInvocationAdapter(), transport, httpClient, pipelineFactory, loggerFactory))
                 {
-                    //TODO: Get rid of this. This is to prevent "No channel" failures due to sends occuring before the first poll.
-                    await Task.Delay(500);
                     var result = await connection.Invoke<string>("Echo", originalMessage);
 
                     Assert.Equal(originalMessage, result);
@@ -94,8 +95,6 @@ namespace Microsoft.AspNetCore.SignalR.Client.FunctionalTests
                         tcs.TrySetResult((string)a[0]);
                     });
 
-                    //TODO: Get rid of this. This is to prevent "No channel" failures due to sends occuring before the first poll.
-                    await Task.Delay(500);
                     await connection.Invoke<Task>("CallEcho", originalMessage);
                     var completed = await Task.WhenAny(Task.Delay(2000), tcs.Task);
                     Assert.True(completed == tcs.Task, "Receive timed out!");
@@ -123,6 +122,74 @@ namespace Microsoft.AspNetCore.SignalR.Client.FunctionalTests
 
                     Assert.Equal(ex.Message, "The hub method '!@#$%' could not be resolved.");
                 }
+            }
+        }
+
+        [Fact]
+        public async Task CanSendSendBeforePoll()
+        {
+            using (var httpClient = _testServer.CreateClient())
+            {
+                var resp = await httpClient.GetAsync("http://test/hubs/getid");
+                var connectionId = await resp.Content.ReadAsStringAsync();
+
+                var invocationAdapter = new JsonNetInvocationAdapter();
+                var request = new InvocationDescriptor
+                {
+                    Method = "Echo",
+                    Arguments = new object[] { "Hi" }
+                };
+
+                var stream = new MemoryStream();
+                await invocationAdapter.WriteMessageAsync(request, stream);
+                stream.Position = 0;
+
+                var sendTask = httpClient.PostAsync($"http://test/hubs/send?id={connectionId}",
+                    new StreamContent(stream));
+
+                await Task.Delay(100);
+                var pollTask = httpClient.PostAsync($"http://test/hubs/poll?id={connectionId}", null);
+                await Task.WhenAll(sendTask, pollTask);
+
+                var mockBinder = new Mock<IInvocationBinder>();
+                mockBinder.Setup(b => b.GetReturnType(It.IsAny<string>())).Returns(typeof(string));
+                var result = (InvocationResultDescriptor)await invocationAdapter.ReadMessageAsync(await pollTask.Result.Content.ReadAsStreamAsync(),
+                    mockBinder.Object, CancellationToken.None);
+
+                Assert.Null(result.Error);
+                Assert.Equal("Hi", result.Result);
+            }
+        }
+
+        [Fact]
+        public async Task PendingSendCancelledIfPollNotReceivedWithinTimeout()
+        {
+            using (var httpClient = _testServer.CreateClient())
+            {
+                var resp = await httpClient.GetAsync("http://test/hubs/getid");
+                var connectionId = await resp.Content.ReadAsStringAsync();
+
+                var invocationAdapter = new JsonNetInvocationAdapter();
+                var request = new InvocationDescriptor
+                {
+                    Method = "Echo",
+                    Arguments = new object[] { "Hi" }
+                };
+
+                var stream = new MemoryStream();
+                await invocationAdapter.WriteMessageAsync(request, stream);
+                stream.Position = 0;
+
+                var sendTask = httpClient.PostAsync($"http://test/hubs/send?id={connectionId}",
+                    new StreamContent(stream));
+
+                var timeoutTask = Task.Delay(10000);
+
+                var completedTask = await Task.WhenAny(sendTask, timeoutTask);
+                Assert.Same(sendTask, completedTask);
+                Assert.Equal(
+                    (await Assert.ThrowsAsync<InvalidOperationException>(async () => await sendTask)).Message,
+                    "No channel");
             }
         }
 
