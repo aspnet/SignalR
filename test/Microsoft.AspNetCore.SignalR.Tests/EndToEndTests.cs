55c19f904f9723d7d282535f7689f932580cee01
Mikael Mengistu <mimengis@microsoft.com>
2017-01-12 11:11:06 -0800

    Use newer version of System.Net.WebSockets.Client

--- test/Microsoft.AspNetCore.SignalR.Tests/EndToEndTests.cs
+++ test/Microsoft.AspNetCore.SignalR.Tests/EndToEndTests.cs
@@ -6,6 +6,7 @@ using System.Net.WebSockets;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Testing.xunit;
 using Xunit;
 
 namespace Microsoft.AspNetCore.SignalR.Tests
