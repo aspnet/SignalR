38216c03204beee92d36ea0d369769538723a91a
Andrew Stanton-Nurse <andrew@andrewnurse.net>
2017-02-14 12:15:25 -0800

    clarify out parameter value

--- src/Microsoft.AspNetCore.Sockets.Common/TextMessageFormatter.cs
+++ src/Microsoft.AspNetCore.Sockets.Common/TextMessageFormatter.cs
@@ -1,4 +1,4 @@
-// Copyright (c) .NET Foundation. All rights reserved.
+﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
@@ -219,7 +219,7 @@ namespace Microsoft.AspNetCore.Sockets
                     messageType = MessageType.Error;
                     return true;
                 default:
-                    messageType = MessageType.Text;
+                    messageType = default(MessageType);
                     return false;
             }
         }
