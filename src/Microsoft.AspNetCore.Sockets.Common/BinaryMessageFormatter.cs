c90309d5ebb1ffd8d7991d30ef903f3f5568d933
Andrew Stanton-Nurse <andrew@andrewnurse.net>
2017-02-14 17:01:19 -0800

    clean up to satisfy @BrennanConroy

--- src/Microsoft.AspNetCore.Sockets.Common/BinaryMessageFormatter.cs
+++ src/Microsoft.AspNetCore.Sockets.Common/BinaryMessageFormatter.cs
@@ -1,4 +1,7 @@
-﻿using System;
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
 using System.Binary;
 using System.IO.Pipelines;
 
@@ -15,14 +18,14 @@ namespace Microsoft.AspNetCore.Sockets
         {
             // We can check the size needed right up front!
             var sizeNeeded = sizeof(long) + 1 + message.Payload.Buffer.Length;
-            if(buffer.Length < sizeNeeded)
+            if (buffer.Length < sizeNeeded)
             {
                 bytesWritten = 0;
                 return false;
             }
 
             buffer.WriteBigEndian((long)message.Payload.Buffer.Length);
-            if(!TryFormatType(message.Type, buffer.Slice(sizeof(long), 1)))
+            if (!TryFormatType(message.Type, buffer.Slice(sizeof(long), 1)))
             {
                 bytesWritten = 0;
                 return false;
@@ -38,7 +41,7 @@ namespace Microsoft.AspNetCore.Sockets
         internal static bool TryParseMessage(ReadOnlySpan<byte> buffer, out Message message, out int bytesConsumed)
         {
             // Check if we have enough to read the size and type flag
-            if(buffer.Length < sizeof(long) + 1)
+            if (buffer.Length < sizeof(long) + 1)
             {
                 message = default(Message);
                 bytesConsumed = 0;
@@ -49,13 +52,13 @@ namespace Microsoft.AspNetCore.Sockets
             // REVIEW: We don't really have a primitive for storing that much data. For now, I'm using it
             // REVIEW: but throwing if the size is over 2GB.
             var longLength = buffer.ReadBigEndian<long>();
-            if(longLength > Int32.MaxValue)
+            if (longLength > Int32.MaxValue)
             {
                 throw new FormatException("Messages over 2GB in size are not supported");
             }
             var length = (int)longLength;
 
-            if(!TryParseType(buffer[sizeof(long)], out var messageType))
+            if (!TryParseType(buffer[sizeof(long)], out var messageType))
             {
                 message = default(Message);
                 bytesConsumed = 0;
@@ -63,7 +66,7 @@ namespace Microsoft.AspNetCore.Sockets
             }
 
             // Check if we actually have the whole payload
-            if(buffer.Length < sizeof(long) + 1 + length)
+            if (buffer.Length < sizeof(long) + 1 + length)
             {
                 message = default(Message);
                 bytesConsumed = 0;
@@ -83,7 +86,7 @@ namespace Microsoft.AspNetCore.Sockets
 
         private static bool TryParseType(byte type, out MessageType messageType)
         {
-            switch(type)
+            switch (type)
             {
                 case TextTypeFlag:
                     messageType = MessageType.Text;
