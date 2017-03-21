0746fd9a4cd242f911f82b84cd51563aa2edeaab
Mikael Mengistu <mimengis@microsoft.com>
2017-03-21 11:45:20 -0700

    Sever-Sent Events Transport + Unit Tests + Functional Test

--- src/Microsoft.AspNetCore.Sockets.Common/Internal/Formatters/ServerSentEventsMessageFormatter.cs
+++ src/Microsoft.AspNetCore.Sockets.Common/Internal/Formatters/ServerSentEventsMessageFormatter.cs
@@ -14,11 +14,11 @@ namespace Microsoft.AspNetCore.Sockets.Internal.Formatters
         private static readonly byte[] DataPrefix = new byte[] { (byte)'d', (byte)'a', (byte)'t', (byte)'a', (byte)':', (byte)' ' };
         private static readonly byte[] Newline = new byte[] { (byte)'\r', (byte)'\n' };
 
-        private const byte LineFeed = (byte)'\n';
-        private const char TextTypeFlag = 'T';
-        private const char BinaryTypeFlag = 'B';
-        private const char CloseTypeFlag = 'C';
-        private const char ErrorTypeFlag = 'E';
+        static readonly byte LineFeed = (byte)'\n';
+        static readonly char TextTypeFlag = 'T';
+        static readonly char BinaryTypeFlag = 'B';
+        static readonly char CloseTypeFlag = 'C';
+        static readonly char ErrorTypeFlag = 'E';
 
         public static bool TryWriteMessage(Message message, IOutput output)
         {
