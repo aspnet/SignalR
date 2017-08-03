db5d2c5c71116eaa9dc043063bd6e2a86a132594
Mikael Mengistu <mimengis@microsoft.com>
2017-08-03 16:04:04 -0700

    Hub<T>

---
+++ samples/SocketsSample/Hubs/TestHub.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace SocketsSample.Hubs
+{
+    public class TestHub : Hub<IChatClient>
+    {
+        public override Task OnConnectedAsync()
+        {
+            Clients.All.JoinChat();
+            return Task.CompletedTask;
+        }
+    }
+
+    public interface IChatClient
+    {
+        void JoinChat();
+        void LeaveChat();
+        void SendHello();
+    }
+}
---
+++ src/Microsoft.AspNetCore.SignalR/Hub'T.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.AspNetCore.SignalR
+{
+    public class Hub<T> : Hub where T : class
+    {
+        private IHubClients<T> _clients;
+
+        public new IHubClients<T> Clients
+        {
+            get
+            {
+                return _clients ?? new TypedHubClients<T>(base.Clients);
+            }
+            set { _clients = value; }
+        }
+       
+
+    }
+}
