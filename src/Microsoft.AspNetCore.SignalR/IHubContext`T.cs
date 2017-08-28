c1c0801e74fde1fa62e8a09d4231032dc4a7edc6
FTWinston <ftwinston@gmail.com>
2017-08-29 00:31:12 +0100

    Added typed HubContext

---
+++ src/Microsoft.AspNetCore.SignalR/IHubContext`T.cs
@@ -0,0 +1,19 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Microsoft.AspNetCore.SignalR
+{
+	public interface IHubContext<THub, T>
+		where THub : Hub<T>
+		where T : class
+	{
+		IHubClients<T> Clients { get; }
+
+		IGroupManager Groups { get; }
+	}
+}
