d0707a1a87738d0e60fbef247a2485163a01d2b5
BrennanConroy <brecon@microsoft.com>
2017-09-18 22:09:20 -0700

    Remove subscriptions

--- client-ts/Microsoft.AspNetCore.SignalR.Client.TS/Observable.ts
+++ client-ts/Microsoft.AspNetCore.SignalR.Client.TS/Observable.ts
@@ -8,6 +8,7 @@ export interface Observer<T> {
     next: (value: T) => void;
     error: (err: any) => void;
     complete: () => void;
+    isCompleted?: boolean;
 }
 
 export interface Observable<T> {
@@ -23,24 +24,52 @@ export class Subject<T> implements Observable<T> {
     }
 
     public next(item: T): void {
-        for (let observer of this.observers) {
-            observer.next(item);
+        // loop backwards because array.splice can happen
+        // and a backwards loop avoids missing items
+        for (let i = this.observers.length - 1; i >= 0; i--) {
+            let observer = this.observers[i];
+            if (observer.closed === true) {
+                continue;
+            }
+            else {
+                observer.next(item);
+            }
+
+            if (observer.closed === true) {
+                this.unsubscribe(observer);
+            }
         }
     }
 
     public error(err: any): void {
-        for (let observer of this.observers) {
-            observer.error(err);
+        // loop backwards because array.splice can happen
+        // and a backwards loop avoids missing items
+        for (let i = this.observers.length - 1; i >= 0; i--) {
+            this.observers[i].error(err);
         }
     }
 
     public complete(): void {
-        for (let observer of this.observers) {
-            observer.complete();
+        // loop backwards because array.splice can happen
+        // and a backwards loop avoids missing items
+        for (let i = this.observers.length - 1; i >= 0; i--) {
+            this.unsubscribe(this.observers[i]);
         }
     }
 
     public subscribe(observer: Observer<T>): void {
         this.observers.push(observer);
+    }
+
+    public unsubscribe(observer: Observer<T>): void {
+        if (observer.isCompleted !== true) {
+            observer.isCompleted = true;
+            let index = this.observers.indexOf(observer);
+            if (index !== -1) {
+                this.observers.splice(index, 1);
+                // TODO: send some kind of completion to let server know
+                observer.complete();
+            }
+        }
     }
 }
