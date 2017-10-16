517cb00108f3d495382eaf3f70fc23b543524d48
BrennanConroy <brecon@microsoft.com>
2017-10-16 14:08:49 -0700

    feedback

--- src/Microsoft.AspNetCore.SignalR.Redis/RedisHubLifetimeManager.cs
+++ src/Microsoft.AspNetCore.SignalR.Redis/RedisHubLifetimeManager.cs
@@ -119,23 +119,21 @@ namespace Microsoft.AspNetCore.SignalR.Redis
             {
                 var groupMessage = DeserializeMessage<GroupMessage>(data);
 
+                var connection = _connections[groupMessage.ConnectionId];
+                if (connection == null)
+                {
+                    // user not on this server
+                    return;
+                }
+
                 if (groupMessage.Action == GroupAction.Remove)
                 {
-                    var connection = _connections[groupMessage.ConnectionId];
-                    if (connection == null || !(await RemoveGroupAsyncCore(connection, groupMessage.Group)))
-                    {
-                        // user not on this server
-                        return;
-                    }
+                    await RemoveGroupAsyncCore(connection, groupMessage.Group);
                 }
 
                 if (groupMessage.Action == GroupAction.Add)
                 {
-                    if (!await AddGroupAsyncCore(groupMessage.ConnectionId, groupMessage.Group))
-                    {
-                        // user not on this server
-                        return;
-                    }
+                    await AddGroupAsyncCore(connection, groupMessage.Group);
                 }
 
                 // Sending ack to server that sent the original add/remove
@@ -322,23 +320,19 @@ namespace Microsoft.AspNetCore.SignalR.Redis
                 throw new ArgumentNullException(nameof(groupName));
             }
 
-            if (await AddGroupAsyncCore(connectionId, groupName))
+            var connection = _connections[connectionId];
+            if (connection != null)
             {
                 // short circuit if connection is on this server
+                await AddGroupAsyncCore(connection, groupName);
                 return;
             }
 
             await SendGroupActionAndWaitForAck(connectionId, groupName, GroupAction.Add);
         }
 
-        private async Task<bool> AddGroupAsyncCore(string connectionId, string groupName)
+        private async Task AddGroupAsyncCore(HubConnectionContext connection, string groupName)
         {
-            var connection = _connections[connectionId];
-            if (connection == null)
-            {
-                return false;
-            }
-
             var feature = connection.Features.Get<IRedisFeature>();
             var groupNames = feature.Groups;
 
@@ -358,7 +352,7 @@ namespace Microsoft.AspNetCore.SignalR.Redis
                 // Subscribe once
                 if (group.Connections.Count > 1)
                 {
-                    return true;
+                    return;
                 }
 
                 var previousTask = Task.CompletedTask;
@@ -386,8 +380,6 @@ namespace Microsoft.AspNetCore.SignalR.Redis
             {
                 group.Lock.Release();
             }
-
-            return true;
         }
 
         public override async Task RemoveGroupAsync(string connectionId, string groupName)
@@ -404,9 +396,10 @@ namespace Microsoft.AspNetCore.SignalR.Redis
 
 
             var connection = _connections[connectionId];
-            if (connection != null && await RemoveGroupAsyncCore(connection, groupName))
+            if (connection != null)
             {
                 // short circuit if connection is on this server
+                await RemoveGroupAsyncCore(connection, groupName);
                 return;
             }
 
@@ -417,14 +410,14 @@ namespace Microsoft.AspNetCore.SignalR.Redis
         /// This takes <see cref="HubConnectionContext"/> because we want to remove the connection from the
         /// _connections list in OnDisconnectedAsync and still be able to remove groups with this method.
         /// </summary>
-        private async Task<bool> RemoveGroupAsyncCore(HubConnectionContext connection, string groupName)
+        private async Task RemoveGroupAsyncCore(HubConnectionContext connection, string groupName)
         {
             var groupChannel = _channelNamePrefix + ".group." + groupName;
 
             GroupData group;
             if (!_groups.TryGetValue(groupChannel, out group))
             {
-                return false;
+                return;
             }
 
             var feature = connection.Features.Get<IRedisFeature>();
@@ -456,7 +449,7 @@ namespace Microsoft.AspNetCore.SignalR.Redis
                 group.Lock.Release();
             }
 
-            return true;
+            return;
         }
 
         private async Task SendGroupActionAndWaitForAck(string connectionId, string groupName, GroupAction action)
