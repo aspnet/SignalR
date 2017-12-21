c7efbb5eb5592c5825b826cfb52b9c384e18c231
Andrew Stanton-Nurse <andrew@andrewnurse.net>
2017-12-21 12:04:37 -0800

    picked nits

--- client-ts/Microsoft.AspNetCore.SignalR.Client.TS.Tests/HttpClient.spec.ts
+++ client-ts/Microsoft.AspNetCore.SignalR.Client.TS.Tests/HttpClient.spec.ts
@@ -1,3 +1,6 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
 import { asyncit as it } from "./Utils"
 import { TestHttpClient } from "./TestHttpClient";
 import { HttpRequest } from "../Microsoft.AspNetCore.SignalR.Client.TS/index";
--- client-ts/Microsoft.AspNetCore.SignalR.Client.TS/AbortController.ts
+++ client-ts/Microsoft.AspNetCore.SignalR.Client.TS/AbortController.ts
@@ -1,3 +1,6 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
 // Rough polyfill of https://developer.mozilla.org/en-US/docs/Web/API/AbortController
 // We don't actually ever use the API being polyfilled, we always use the polyfill because
 // it's a very new API right now.
