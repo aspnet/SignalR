13836b6caa775714cb439b554fec8be4140d47f7
Josh Williams <williams.jackj@gmail.com>
2017-12-21 14:46:38 -0600

    Throw InvalidDataException instead of FormatException in NegotationProtocol  - Modify NegotiationProtocol to throw InvalidDataException  - Update NegotiationProtocolTests expectations  - Remove test case for InlineData "Missing required property 'protocol'"
    Addresses #1203

--- test/Microsoft.AspNetCore.SignalR.Common.Tests/Internal/Protocol/NegotiationProtocolTests.cs
+++ test/Microsoft.AspNetCore.SignalR.Common.Tests/Internal/Protocol/NegotiationProtocolTests.cs
@@ -30,13 +30,12 @@ namespace Microsoft.AspNetCore.SignalR.Common.Tests.Internal.Protocol
         [InlineData("42\u001e", "Unexpected JSON Token Type 'Integer'. Expected a JSON Object.")]
         [InlineData("\"42\"\u001e", "Unexpected JSON Token Type 'String'. Expected a JSON Object.")]
         [InlineData("null\u001e", "Unexpected JSON Token Type 'Null'. Expected a JSON Object.")]
-        [InlineData("{}\u001e", "Missing required property 'protocol'.")]
         [InlineData("[]\u001e", "Unexpected JSON Token Type 'Array'. Expected a JSON Object.")]
         public void ParsingNegotiationMessageThrowsForInvalidMessages(string payload, string expectedMessage)
         {
             var message = Encoding.UTF8.GetBytes(payload);
 
-            var exception = Assert.Throws<FormatException>(() =>
+            var exception = Assert.Throws<InvalidDataException>(() =>
                 Assert.True(NegotiationProtocol.TryParseMessage(message, out var deserializedMessage)));
 
             Assert.Equal(expectedMessage, exception.Message);
