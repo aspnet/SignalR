255108320dba48379ec67c1db52dc43f075fdabb
Mikael Mengistu <mimengis@microsoft.com>
2018-01-02 14:06:01 -0800

    PR comments

--- src/Microsoft.AspNetCore.SignalR.Core/HubContext.cs
+++ src/Microsoft.AspNetCore.SignalR.Core/HubContext.cs
@@ -8,14 +8,16 @@ namespace Microsoft.AspNetCore.SignalR
     public class HubContext<THub> : IHubContext<THub> where THub : Hub
     {
         private readonly HubLifetimeManager<THub> _lifetimeManager;
+        private readonly IHubClients _clients;
 
         public HubContext(HubLifetimeManager<THub> lifetimeManager)
         {
             _lifetimeManager = lifetimeManager;
+            _clients = new HubClients<THub>(_lifetimeManager);
             Groups = new GroupManager<THub>(lifetimeManager);
         }
 
-        public IHubClients Clients => new HubClients<THub>(_lifetimeManager);
+        public IHubClients Clients => _clients;
 
         public virtual IGroupManager Groups { get; }
     }
