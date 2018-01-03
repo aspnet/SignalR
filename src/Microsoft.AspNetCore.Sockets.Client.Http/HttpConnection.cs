ff763b0a94c27eae1df962b02f8285a7ac23dc76
BrennanConroy <brecon@microsoft.com>
2018-01-03 14:41:34 -0800

    nit

--- src/Microsoft.AspNetCore.Sockets.Client.Http/HttpConnection.cs
+++ src/Microsoft.AspNetCore.Sockets.Client.Http/HttpConnection.cs
@@ -28,6 +28,7 @@ namespace Microsoft.AspNetCore.Sockets.Client
         private readonly ILoggerFactory _loggerFactory;
         private readonly ILogger _logger;
         private volatile ConnectionState _connectionState = ConnectionState.Disconnected;
+        private readonly object _stateChangeLock = new object();
         private readonly HttpClient _httpClient;
         private readonly HttpOptions _httpOptions;
         private readonly List<ReceiveCallback> _callbacks = new List<ReceiveCallback>();
@@ -35,7 +36,6 @@ namespace Microsoft.AspNetCore.Sockets.Client
         private readonly ConnectionLogScope _logScope;
         private readonly IDisposable _scopeDisposable;
         private readonly ITransportFactory _transportFactory;
-        private readonly object _stateChangeLock = new object();
 
         private HttpConnectionOneOff _connection;
         private List<Action<Exception>> _closed = new List<Action<Exception>>();
