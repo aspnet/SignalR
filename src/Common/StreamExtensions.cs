682789d542a28e31a6e14b66ca571664ed73963d
David Fowler <davidfowl@gmail.com>
2018-02-19 15:15:31 -0800

    Tackling some low hanging performance fruit - Use native Memory/Span APIs on Stream and WebSocket in .NET Core 2.1 - Remove double copying in formatters - Implemented custom HttpContent over ReadOnlyBuffer<byte>

---
+++ src/Common/StreamExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System.Buffers;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+namespace System.IO
+{
+    internal static class StreamExtensions
+    {
+        public static async Task WriteAsync(this Stream stream, ReadOnlyBuffer<byte> buffer)
+        {
+            // REVIEW: Should we special case IsSingleSegment here?
+            foreach (var segment in buffer)
+            {
+#if NETCOREAPP2_1
+                await stream.WriteAsync(segment);
+#else
+                var isArray = MemoryMarshal.TryGetArray(segment, out var arraySegment);
+                // We're using the managed memory pool which is backed by managed buffers
+                Debug.Assert(isArray);
+                await stream.WriteAsync(arraySegment.Array, arraySegment.Offset, arraySegment.Count);
+#endif
+            }
+        }
+    }
+}
---
+++ src/Common/WebSocketExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Buffers;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace System.Net.WebSockets
+{
+    internal static class WebSocketExtensions
+    {
+        public static async Task SendAsync(this WebSocket webSocket, ReadOnlyBuffer<byte> buffer, WebSocketMessageType webSocketMessageType, CancellationToken cancellationToken = default)
+        {
+            // TODO: Consider chunking writes here if we get a multi segment buffer
+#if NETCOREAPP2_1
+            if (buffer.IsSingleSegment)
+            {
+                await webSocket.SendAsync(buffer.First, webSocketMessageType, endOfMessage: true, CancellationToken.None);
+            }
+            else
+            {
+                await webSocket.SendAsync(buffer.ToArray(), webSocketMessageType, endOfMessage: true, CancellationToken.None);
+            }
+#else
+            if (buffer.IsSingleSegment)
+            {
+                var isArray = MemoryMarshal.TryGetArray(buffer.First, out var segment);
+                Debug.Assert(isArray);
+                await webSocket.SendAsync(segment, webSocketMessageType, endOfMessage: true, CancellationToken.None);
+            }
+            else
+            {
+                await webSocket.SendAsync(new ArraySegment<byte>(buffer.ToArray()), webSocketMessageType, true, CancellationToken.None);
+            }
+#endif
+        }
+    }
+}
