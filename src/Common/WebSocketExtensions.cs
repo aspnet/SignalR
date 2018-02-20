2f79c05e01b40138fe9cbaafd8da8548b41f137e
David Fowler <davidfowl@gmail.com>
2018-02-20 09:52:53 -0800

    PR comments

--- src/Common/WebSocketExtensions.cs
+++ src/Common/WebSocketExtensions.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
 using System.Buffers;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -11,28 +14,28 @@ namespace System.Net.WebSockets
 {
     internal static class WebSocketExtensions
     {
-        public static async Task SendAsync(this WebSocket webSocket, ReadOnlyBuffer<byte> buffer, WebSocketMessageType webSocketMessageType, CancellationToken cancellationToken = default)
+        public static Task SendAsync(this WebSocket webSocket, ReadOnlyBuffer<byte> buffer, WebSocketMessageType webSocketMessageType, CancellationToken cancellationToken = default)
         {
             // TODO: Consider chunking writes here if we get a multi segment buffer
 #if NETCOREAPP2_1
             if (buffer.IsSingleSegment)
             {
-                await webSocket.SendAsync(buffer.First, webSocketMessageType, endOfMessage: true, CancellationToken.None);
+                return webSocket.SendAsync(buffer.First, webSocketMessageType, endOfMessage: true, cancellationToken);
             }
             else
             {
-                await webSocket.SendAsync(buffer.ToArray(), webSocketMessageType, endOfMessage: true, CancellationToken.None);
+                return webSocket.SendAsync(buffer.ToArray(), webSocketMessageType, endOfMessage: true, cancellationToken);
             }
 #else
             if (buffer.IsSingleSegment)
             {
                 var isArray = MemoryMarshal.TryGetArray(buffer.First, out var segment);
                 Debug.Assert(isArray);
-                await webSocket.SendAsync(segment, webSocketMessageType, endOfMessage: true, CancellationToken.None);
+                return webSocket.SendAsync(segment, webSocketMessageType, endOfMessage: true, cancellationToken);
             }
             else
             {
-                await webSocket.SendAsync(new ArraySegment<byte>(buffer.ToArray()), webSocketMessageType, true, CancellationToken.None);
+                return webSocket.SendAsync(new ArraySegment<byte>(buffer.ToArray()), webSocketMessageType, true, cancellationToken);
             }
 #endif
         }
