8cf2cde16d3aaf7babcdf0662ddd66da72575347
James Newton-King <james@newtonking.com>
2018-03-07 20:58:23 +1300

    Client sends user agent with version based on assembly version

--- src/Microsoft.AspNetCore.Sockets.Client.Http/Constants.cs
+++ src/Microsoft.AspNetCore.Sockets.Client.Http/Constants.cs
@@ -1,13 +1,25 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System.Linq;
 using System.Net.Http.Headers;
+using System.Reflection;
 
 namespace Microsoft.AspNetCore.Sockets.Client.Http
 {
-    public class Constants
+    public static class Constants
     {
-        private static readonly string UserAgent = "Microsoft.AspNetCore.Sockets.Client.Http/1.0.0-alpha";
-        public static readonly ProductInfoHeaderValue UserAgentHeader = ProductInfoHeaderValue.Parse(UserAgent);
+        public static readonly ProductInfoHeaderValue UserAgentHeader;
+
+        static Constants()
+        {
+            var assemblyVersion = (AssemblyInformationalVersionAttribute)typeof(Constants)
+                .Assembly
+                .GetCustomAttributes(typeof(AssemblyInformationalVersionAttribute))
+                .Single();
+
+            var userAgent = "Microsoft.AspNetCore.Sockets.Client.Http/" + assemblyVersion.InformationalVersion;
+            UserAgentHeader = ProductInfoHeaderValue.Parse(userAgent);
+        }
     }
 }
