678d70c150ae6ba5d17513b9a60511a2ff8b4b77
BrennanConroy <brecon@microsoft.com>
2018-03-12 14:18:21 -0700

    Add benchmark application to run via aspnet/benchmarks

---
+++ benchmarks/BenchmarkServer/Startup.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using BenchmarkServer.Hubs;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BenchmarkServer
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSignalR();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseSignalR(routes =>
+            {
+                routes.MapHub<EchoHub>("/default");
+            });
+        }
+    }
+}
