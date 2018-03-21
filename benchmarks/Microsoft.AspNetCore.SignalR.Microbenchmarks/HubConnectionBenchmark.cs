2759d3d1ee374458c97443938596207582dd0128
James Newton-King <james@newtonking.com>
2018-03-22 11:30:00 +1300

    Fix benchmark

--- benchmarks/Microsoft.AspNetCore.SignalR.Microbenchmarks/HubConnectionBenchmark.cs
+++ benchmarks/Microsoft.AspNetCore.SignalR.Microbenchmarks/HubConnectionBenchmark.cs
@@ -27,51 +27,51 @@ namespace Microsoft.AspNetCore.SignalR.Microbenchmarks
             _hubConnection = new HubConnection(new TestConnection(), new JsonHubProtocol(), new NullLoggerFactory());
         }
 
-        private class TestConnection : IConnection
-        {
-            public Task StartAsync(TransferFormat transferFormat)
-            {
-                return Task.CompletedTask;
-            }
-
-            public Task SendAsync(byte[] data, CancellationToken cancellationToken)
-            {
-                return Task.CompletedTask;
-            }
-
-            public Task StopAsync()
-            {
-                return Task.CompletedTask;
-            }
-
-            public Task DisposeAsync()
-            {
-                return Task.CompletedTask;
-            }
-
-            public Task AbortAsync(Exception ex)
-            {
-                return Task.CompletedTask;
-            }
-
-            public IDisposable OnReceived(Func<byte[], object, Task> callback, object state)
-            {
-                return Task.CompletedTask;
-            }
-
-            public event Action<Exception> Closed;
-            public IFeatureCollection Features { get; }
-
-            protected virtual void OnClosed(Exception obj)
-            {
-                Closed?.Invoke(obj);
-            }
-        }
-
         [Benchmark]
         public Task StartAsync()
         {
             return _hubConnection.StartAsync();
+        }
+    }
+
+    public class TestConnection : IConnection
+    {
+        public Task StartAsync(TransferFormat transferFormat)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task SendAsync(byte[] data, CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync()
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task DisposeAsync()
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task AbortAsync(Exception ex)
+        {
+            return Task.CompletedTask;
+        }
+
+        public IDisposable OnReceived(Func<byte[], object, Task> callback, object state)
+        {
+            return Task.CompletedTask;
+        }
+
+        public event Action<Exception> Closed;
+        public IFeatureCollection Features { get; }
+
+        protected virtual void OnClosed(Exception obj)
+        {
+            Closed?.Invoke(obj);
         }
     }
 }
--- src/Microsoft.AspNetCore.SignalR.Common/Internal/Protocol/LimitArrayPoolWriteStream.cs
+++ src/Microsoft.AspNetCore.SignalR.Common/Internal/Protocol/LimitArrayPoolWriteStream.cs
@@ -145,7 +145,11 @@ namespace Microsoft.AspNetCore.SignalR.Internal.Protocol
         public override bool CanRead => false;
         public override bool CanSeek => false;
 
-        public override long Position { get { throw new NotSupportedException(); } set { throw new NotSupportedException(); } }
+        public override long Position
+        {
+            get => throw new NotSupportedException();
+            set => throw new NotSupportedException();
+        }
         public override int Read(byte[] buffer, int offset, int count) { throw new NotSupportedException(); }
         public override long Seek(long offset, SeekOrigin origin) { throw new NotSupportedException(); }
         public override void SetLength(long value) { throw new NotSupportedException(); }
