881edd9412431bea17ef49e22c4f8141053c549f
David Fowler <davidfowl@gmail.com>
2018-03-26 01:03:53 -0700

    Changed Utf8BufferTextReader to use the Utf8Decoder - It copies whatever it can into the char buffer allocated in a stateful way (it's more efficient). - Added tests for unicode and ascii reading

--- src/Microsoft.AspNetCore.SignalR.Common/Internal/Protocol/Utf8BufferTextReader.cs
+++ src/Microsoft.AspNetCore.SignalR.Common/Internal/Protocol/Utf8BufferTextReader.cs
@@ -11,10 +11,12 @@ namespace Microsoft.AspNetCore.SignalR.Internal.Protocol
     internal class Utf8BufferTextReader : TextReader
     {
         private ReadOnlyMemory<byte> _utf8Buffer;
+        private Decoder _decoder;
 
         public Utf8BufferTextReader(ReadOnlyMemory<byte> utf8Buffer)
         {
             _utf8Buffer = utf8Buffer;
+            _decoder = Encoding.UTF8.GetDecoder();
         }
 
         public override int Read(char[] buffer, int index, int count)
@@ -25,32 +27,24 @@ namespace Microsoft.AspNetCore.SignalR.Internal.Protocol
             }
 
             var source = _utf8Buffer.Span;
-            var destinationBytesCount = Encoding.UTF8.GetByteCount(buffer, index, count);
-
-            // We have then the destination
-            if (source.Length > destinationBytesCount)
-            {
-                source = source.Slice(0, destinationBytesCount);
-
-                _utf8Buffer = _utf8Buffer.Slice(destinationBytesCount);
-            }
-            else
-            {
-                _utf8Buffer = ReadOnlyMemory<byte>.Empty;
-            }
-
+            var bytesUsed = 0;
+            var charsUsed = 0;
 #if NETCOREAPP2_1
-            return Encoding.UTF8.GetChars(source, new Span<char>(buffer, index, count));
+            var destination = new Span<char>(buffer, index, count);
+            _decoder.Convert(source, destination, true, out bytesUsed, out charsUsed, out var completed);
 #else
             unsafe
             {
                 fixed (char* destinationChars = &buffer[index])
                 fixed (byte* sourceBytes = &MemoryMarshal.GetReference(source))
                 {
-                    return Encoding.UTF8.GetChars(sourceBytes, source.Length, destinationChars, count);
+                    _decoder.Convert(sourceBytes, source.Length, destinationChars, count, true, out bytesUsed, out charsUsed, out var completed);
                 }
             }
 #endif
+            _utf8Buffer = _utf8Buffer.Slice(bytesUsed);
+            
+            return charsUsed;
         }
     }
 }
