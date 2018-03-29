8b6c6d0bcfcfb01990c7ab8910f4fda62953519b
BrennanConroy <brecon@microsoft.com>
2018-03-28 22:44:50 -0700

    copyright

--- clients/cpp/samples/HubConnectionSample/stdafx.h
+++ clients/cpp/samples/HubConnectionSample/stdafx.h
@@ -1,2 +1,4 @@
-#pragma once
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+#pragma once
