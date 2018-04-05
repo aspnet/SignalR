28c86bbcf563b69a7986deaac05222da694aed11
Pranav K <prkrishn@hotmail.com>
2018-04-05 09:58:02 -0700

    Re-enable Windows dotnet-ci builds

--- build/buildpipeline/windows.groovy
+++ build/buildpipeline/windows.groovy
@@ -1,2 +1,12 @@
 @Library('dotnet-ci') _
 
+// 'node' indicates to Jenkins that the enclosed block runs on a node that matches
+// the label 'windows-with-vs'
+simpleNode('Windows.10.Enterprise.RS3.ASPNET') {
+    stage ('Checking out source') {
+        checkout scm
+    }
+    stage ('Build') {
+        bat '.\\run.cmd -CI default-build'
+    }
+}
