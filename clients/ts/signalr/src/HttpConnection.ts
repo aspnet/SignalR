b705156b0e890fb191cf2a088f3687c5ea95fcfc
unknown <mimengis@microsoft.com>
2018-04-04 21:23:06 -0700

    + comment

--- clients/ts/signalr/src/HttpConnection.ts
+++ clients/ts/signalr/src/HttpConnection.ts
@@ -272,6 +272,11 @@ export class HttpConnection implements IConnection {
             throw new Error(`Cannot resolve '${url}'.`);
         }
 
+        // Setting the url to the href propery of an anchor tag handles normalization
+        // for us. There are 3 main cases.
+        // 1. Relative  path normalization e.g "b" -> "http://localhost:5000/a/b"
+        // 2. Absolute path normalization e.g "/a/b" -> "http://localhost:5000/a/b"
+        // 3. Networkpath reference normalization e.g "//localhost:5000/a/b" -> "http://localhost:5000/a/b"
         const aTag = window.document.createElement("a");
         aTag.href = url;
 
