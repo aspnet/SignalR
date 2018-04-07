9515d2ef1f63b90646f3ecede2eaeca9c2383c62
David Fowler <davidfowl@gmail.com>
2018-04-07 02:12:48 -0700

    Remove usings.

--- src/Microsoft.AspNetCore.Http.Connections.Client/Internal/ClientPipeOptions.cs
+++ src/Microsoft.AspNetCore.Http.Connections.Client/Internal/ClientPipeOptions.cs
@@ -1,12 +1,7 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using System;
 using System.IO.Pipelines;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace Microsoft.AspNetCore.Http.Connections.Client.Internal
 {
