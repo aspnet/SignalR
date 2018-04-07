1dbe41802768457c23f1c231fad563f33a3112ba
David Fowler <davidfowl@gmail.com>
2018-04-07 12:31:10 -0700

    Add sample showing Observable -> ChannelReader adaption

--- samples/SignalRSamples/Hubs/Streaming.cs
+++ samples/SignalRSamples/Hubs/Streaming.cs
@@ -11,6 +11,28 @@ namespace SignalRSamples.Hubs
 {
     public class Streaming : Hub
     {
+        public ChannelReader<int> ObservableCounter(int count, int delay)
+        {
+            // This sample shows adapting an observable to a ChannelReader without 
+            // back pressure, if the connection is slower than the producer, memory will
+            // start to increase.
+            var observable = Observable.Interval(TimeSpan.FromMilliseconds(delay))
+                             .Select((_, index) => index)
+                             .Take(count);
+
+            var channel = Channel.CreateUnbounded<int>();
+
+            var disposable = observable.Subscribe(
+                                value => channel.Writer.TryWrite(value),
+                                error => channel.Writer.TryComplete(error),
+                                () => channel.Writer.TryComplete());
+
+            // Complete the subscription on the reader completing
+            channel.Reader.Completion.ContinueWith(task => disposable.Dispose());
+
+            return channel.Reader;
+        }
+
         public ChannelReader<int> ChannelCounter(int count, int delay)
         {
             var channel = Channel.CreateUnbounded<int>();
