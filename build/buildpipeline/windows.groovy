1b660299791b307fc3007d4feec056f818384a79
Pranav K <prkrishn@hotmail.com>
2018-04-12 11:01:46 -0700

    Let's start from scratch

--- build/buildpipeline/windows.groovy
+++ build/buildpipeline/windows.groovy
@@ -6,11 +6,7 @@ simpleNode('Windows.10.Enterprise.RS3.ASPNET') {
     stage ('Checking out source') {
         checkout scm
     }
-    
     stage ('Build') {
-        def logDir = "./logs"
-        def environment = "set ASPNETCORE_TEST_LOG_DIR=${logDir}"
-        bat "${environment} & .\\run.cmd -CI default-build"
-        archiveArtifacts allowEmptyArchive: false, artifacts: './logs/**'
+        bat '.\\run.cmd -CI default-build'
     }
 }
