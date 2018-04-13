cef3cb3422a8de03578b70e29181a92eea45d03c
Andrew Stanton-Nurse <andrew@stanton-nurse.com>
2018-04-13 11:54:31 -0700

    s/msgpack/messagepack/g

--- src/Microsoft.AspNetCore.SignalR.Protocols.MessagePack/MsgPackProtocolDependencyInjectionExtensions.cs
+++ src/Microsoft.AspNetCore.SignalR.Protocols.MessagePack/MsgPackProtocolDependencyInjectionExtensions.cs
@@ -1,31 +1,30 @@
-using System;
-using System.Linq;
 using Microsoft.AspNetCore.SignalR;
 using Microsoft.AspNetCore.SignalR.Protocol;
 using Microsoft.Extensions.DependencyInjection.Extensions;
+using System;
 
 namespace Microsoft.Extensions.DependencyInjection
 {
-    public static class MsgPackProtocolDependencyInjectionExtensions
+    public static class MessagePackProtocolDependencyInjectionExtensions
     {
         /// <summary>
-        /// Enables the MsgPack protocol for SignalR.
+        /// Enables the MessagePack protocol for SignalR.
         /// </summary>
         /// <remarks>
-        /// This has no effect if the MsgPack protocol has already been enabled.
+        /// This has no effect if the MessagePack protocol has already been enabled.
         /// </remarks>
-        /// <param name="builder">The <see cref="ISignalRBuilder"/> representing the SignalR server to add MsgPack protocol support to.</param>
+        /// <param name="builder">The <see cref="ISignalRBuilder"/> representing the SignalR server to add MessagePack protocol support to.</param>
         /// <returns>The value of <paramref name="builder"/></returns>
         public static TBuilder AddMessagePackProtocol<TBuilder>(this TBuilder builder) where TBuilder : ISignalRBuilder
             => AddMessagePackProtocol(builder, _ => { });
 
         /// <summary>
-        /// Enables the MsgPack protocol for SignalR and allows options for the MsgPack protocol to be configured.
+        /// Enables the MessagePack protocol for SignalR and allows options for the MessagePack protocol to be configured.
         /// </summary>
         /// <remarks>
-        /// Any options configured here will be applied, even if the MsgPack protocol has already been registered with the server.
+        /// Any options configured here will be applied, even if the MessagePack protocol has already been registered.
         /// </remarks>
-        /// <param name="builder">The <see cref="ISignalRBuilder"/> representing the SignalR server to add MsgPack protocol support to.</param>
+        /// <param name="builder">The <see cref="ISignalRBuilder"/> representing the SignalR server to add MessagePack protocol support to.</param>
         /// <param name="configure">A delegate that can be used to configure the <see cref="MessagePackHubProtocolOptions"/></param>
         /// <returns>The value of <paramref name="builder"/></returns>
         public static TBuilder AddMessagePackProtocol<TBuilder>(this TBuilder builder, Action<MessagePackHubProtocolOptions> configure) where TBuilder : ISignalRBuilder
