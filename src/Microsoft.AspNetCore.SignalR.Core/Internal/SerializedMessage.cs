d8d8e24c80c94dd78aef20cdf9a851aba13d7625
David Fowler <davidfowl@gmail.com>
2018-04-12 23:07:02 -0700

    API review changes - Changed GetMessageBytes to return ReadOnlyMemory - Make HandshakeProtocol.SuccessHandshakeData a readonly field

--- src/Microsoft.AspNetCore.SignalR.Core/Internal/SerializedMessage.cs
+++ src/Microsoft.AspNetCore.SignalR.Core/Internal/SerializedMessage.cs
@@ -1,11 +1,13 @@
+using System;
+
 namespace Microsoft.AspNetCore.SignalR.Internal
 {
     public readonly struct SerializedMessage
     {
         public string ProtocolName { get; }
-        public byte[] Serialized { get; }
+        public ReadOnlyMemory<byte> Serialized { get; }
 
-        public SerializedMessage(string protocolName, byte[] serialized)
+        public SerializedMessage(string protocolName, ReadOnlyMemory<byte> serialized)
         {
             ProtocolName = protocolName;
             Serialized = serialized;
