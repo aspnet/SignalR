131cb71b49d46855d8c650c6a72a78fef119127c
Andrew Stanton-Nurse <andrew@stanton-nurse.com>
2018-04-16 20:17:01 -0700

    underp copyright header

--- clients/ts/signalr/src/ITransport.ts
+++ clients/ts/signalr/src/ITransport.ts
@@ -1,7 +1,7 @@
-import { IConnection } from "./IConnection";
-
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+import { IConnection } from "./IConnection";
 
 export enum HttpTransportType {
     WebSockets,
