71ce49afc2f802c33f00db7f8d2b3c8e93b6c807
Andrew Stanton-Nurse <andrew@stanton-nurse.com>
2018-04-19 15:18:36 -0700

    ANE

--- src/Microsoft.AspNetCore.SignalR/GetHttpContextExtensions.cs
+++ src/Microsoft.AspNetCore.SignalR/GetHttpContextExtensions.cs
@@ -1,6 +1,7 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.Connections.Features;
 
@@ -10,11 +11,19 @@ namespace Microsoft.AspNetCore.SignalR
     {
         public static HttpContext GetHttpContext(this HubCallerContext connection)
         {
+            if (connection == null)
+            {
+                throw new ArgumentNullException(nameof(connection));
+            }
             return connection.Features.Get<IHttpContextFeature>()?.HttpContext;
         }
 
         public static HttpContext GetHttpContext(this HubConnectionContext connection)
         {
+            if (connection == null)
+            {
+                throw new ArgumentNullException(nameof(connection));
+            }
             return connection.Features.Get<IHttpContextFeature>()?.HttpContext;
         }
     }
