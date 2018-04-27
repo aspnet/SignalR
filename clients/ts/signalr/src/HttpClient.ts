c64fef1a9fff253c2c1fb0a0930e269ed9f0ef09
Andrew Stanton-Nurse <andrew@stanton-nurse.com>
2018-04-27 12:29:04 -0700

    up to but excluding HubConnectionBuilder

--- clients/ts/signalr/src/HttpClient.ts
+++ clients/ts/signalr/src/HttpClient.ts
@@ -5,30 +5,89 @@ import { AbortSignal } from "./AbortController";
 import { HttpError, TimeoutError } from "./Errors";
 import { ILogger, LogLevel } from "./ILogger";
 
+/** Represents an HTTP request. */
 export interface HttpRequest {
+    /** The HTTP method to use for the request. */
     method?: string;
+
+    /** The URL for the request. */
     url?: string;
+
+    /** The body content for the request. May be a string or an ArrayBuffer (for binary data). */
     content?: string | ArrayBuffer;
+
+    /** An object describing headers to apply to the request. */
     headers?: { [key: string]: string };
+
+    /** The XMLHttpRequestResponseType to apply to the request. */
     responseType?: XMLHttpRequestResponseType;
+
+    /** An AbortSignal that can be monitored for cancellation. */
     abortSignal?: AbortSignal;
+
+    /** The time to wait for the request to complete before throwing a TimeoutError. Measured in milliseconds. */
     timeout?: number;
 }
 
+/** Represents an HTTP response. */
 export class HttpResponse {
+    /** Constructs a new instance of {@link HttpResponse} with the specified status code.
+     *
+     * @param {number} statusCode - The status code of the response.
+     */
     constructor(statusCode: number);
+
+    /** Constructs a new instance of {@link HttpResponse} with the specified status code and message.
+     *
+     * @param {number} statusCode - The status code of the response.
+     * @param {string} statusText - The status message of the response.
+     */
     constructor(statusCode: number, statusText: string);
+
+    /** Constructs a new instance of {@link HttpResponse} with the specified status code, message and string content.
+     *
+     * @param {number} statusCode - The status code of the response.
+     * @param {string} statusText - The status message of the response.
+     * @param {string} content - The content of the response.
+     */
     constructor(statusCode: number, statusText: string, content: string);
+
+    /** Constructs a new instance of {@link HttpResponse} with the specified status code, message and binary content.
+     *
+     * @param {number} statusCode - The status code of the response.
+     * @param {string} statusText - The status message of the response.
+     * @param {ArrayBuffer} content - The content of the response.
+     */
     constructor(statusCode: number, statusText: string, content: ArrayBuffer);
+
     constructor(
+        /** The status code of the response. */
         public readonly statusCode: number,
+        /** The status message of the response. */
         public readonly statusText?: string,
+        /** The body content of the response. */
         public readonly content?: string | ArrayBuffer) {
     }
 }
 
+/** Abstraction over an HTTP client
+ *
+ * This class provides an abstraction over an HTTP client so that a different implementation can be provided on different platforms.
+ */
 export abstract class HttpClient {
+    /** Issues an HTTP GET request to the specified URL, returning a Promise that resolves with an {@link HttpResponse} representing the result.
+     *
+     * @param {string} url The URL for the request.
+     * @returns {Promise<HttpResponse>} A Promise that resolves with an {@link HttpResponse} describing the response, or rejects with an Error indicating a failure.
+     */
     public get(url: string): Promise<HttpResponse>;
+
+    /** Issues an HTTP GET request to the specified URL, returning a Promise that resolves with an {@link HttpResponse} representing the result.
+     *
+     * @param {string} url The URL for the request.
+     * @param {HttpRequest} options Additional options to configure the request. The 'url' field in this object will be overridden by the url parameter.
+     * @returns {Promise<HttpResponse>} A Promise that resolves with an {@link HttpResponse} describing the response, or rejects with an Error indicating a failure.
+     */
     public get(url: string, options: HttpRequest): Promise<HttpResponse>;
     public get(url: string, options?: HttpRequest): Promise<HttpResponse> {
         return this.send({
@@ -38,7 +97,19 @@ export abstract class HttpClient {
         });
     }
 
+    /** Issues an HTTP POST request to the specified URL, returning a Promise that resolves with an {@link HttpResponse} representing the result.
+     *
+     * @param {string} url The URL for the request.
+     * @returns {Promise<HttpResponse>} A Promise that resolves with an {@link HttpResponse} describing the response, or rejects with an Error indicating a failure.
+     */
     public post(url: string): Promise<HttpResponse>;
+
+    /** Issues an HTTP POST request to the specified URL, returning a Promise that resolves with an {@link HttpResponse} representing the result.
+     *
+     * @param {string} url The URL for the request.
+     * @param {HttpRequest} options Additional options to configure the request. The 'url' field in this object will be overridden by the url parameter.
+     * @returns {Promise<HttpResponse>} A Promise that resolves with an {@link HttpResponse} describing the response, or rejects with an Error indicating a failure.
+     */
     public post(url: string, options: HttpRequest): Promise<HttpResponse>;
     public post(url: string, options?: HttpRequest): Promise<HttpResponse> {
         return this.send({
@@ -48,7 +119,19 @@ export abstract class HttpClient {
         });
     }
 
+    /** Issues an HTTP DELETE request to the specified URL, returning a Promise that resolves with an {@link HttpResponse} representing the result.
+     *
+     * @param {string} url The URL for the request.
+     * @returns {Promise<HttpResponse>} A Promise that resolves with an {@link HttpResponse} describing the response, or rejects with an Error indicating a failure.
+     */
     public delete(url: string): Promise<HttpResponse>;
+
+    /** Issues an HTTP DELETE request to the specified URL, returning a Promise that resolves with an {@link HttpResponse} representing the result.
+     *
+     * @param {string} url The URL for the request.
+     * @param {HttpRequest} options Additional options to configure the request. The 'url' field in this object will be overridden by the url parameter.
+     * @returns {Promise<HttpResponse>} A Promise that resolves with an {@link HttpResponse} describing the response, or rejects with an Error indicating a failure.
+     */
     public delete(url: string, options: HttpRequest): Promise<HttpResponse>;
     public delete(url: string, options?: HttpRequest): Promise<HttpResponse> {
         return this.send({
@@ -58,9 +141,15 @@ export abstract class HttpClient {
         });
     }
 
+    /** Issues an HTTP request to the specified URL, returning a {@link Promise} that resolves with an {@link HttpResponse} representing the result.
+     *
+     * @param {HttpRequest} request An {@link HttpRequest} describing the request to send.
+     * @returns {Promise<HttpResponse>} A Promise that resolves with an HttpResponse describing the response, or rejects with an Error indicating a failure.
+     */
     public abstract send(request: HttpRequest): Promise<HttpResponse>;
 }
 
+// Not exported from index.
 export class DefaultHttpClient extends HttpClient {
     private readonly logger: ILogger;
 
