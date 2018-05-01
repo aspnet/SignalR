204e798778c1608243b0bfe8c7a0df489b06562d
Andrew Stanton-Nurse <andrew@stanton-nurse.com>
2018-05-01 12:06:37 -0700

    add comment

--- clients/ts/signalr/src/ITransport.ts
+++ clients/ts/signalr/src/ITransport.ts
@@ -1,6 +1,7 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+// This will be treated as a bit flag in the future, so we keep it using power-of-two values.
 /** Specifies a specific HTTP transport type. */
 export enum HttpTransportType {
     /** Specifies no transport preference. */
