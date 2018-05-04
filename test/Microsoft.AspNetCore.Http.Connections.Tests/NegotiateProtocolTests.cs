71ae5efe0f1c17acf92b1f83af68ef1c6874333f
James Newton-King <james@newtonking.com>
2018-05-04 11:23:59 -0700

    Cleanup

--- test/Microsoft.AspNetCore.Http.Connections.Tests/NegotiateProtocolTests.cs
+++ test/Microsoft.AspNetCore.Http.Connections.Tests/NegotiateProtocolTests.cs
@@ -1,9 +1,10 @@
-using System;
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
-using Microsoft.AspNetCore.Http.Connections.Internal;
 using Microsoft.AspNetCore.Internal;
 using Xunit;
 
