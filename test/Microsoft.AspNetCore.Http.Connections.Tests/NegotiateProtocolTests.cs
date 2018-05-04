d30e482c2fb4a2773b9f395ec089bf2bcf855343
James Newton-King <james@newtonking.com>
2018-05-04 11:18:01 -0700

    Handle null collections when writing NegotiateResponse

--- test/Microsoft.AspNetCore.Http.Connections.Tests/NegotiateProtocolTests.cs
+++ test/Microsoft.AspNetCore.Http.Connections.Tests/NegotiateProtocolTests.cs
@@ -1,7 +1,10 @@
+using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
 using Microsoft.AspNetCore.Http.Connections.Internal;
+using Microsoft.AspNetCore.Internal;
 using Xunit;
 
 namespace Microsoft.AspNetCore.Http.Connections.Tests
@@ -49,6 +52,36 @@ namespace Microsoft.AspNetCore.Http.Connections.Tests
             var exception = Assert.Throws<InvalidDataException>(() => NegotiateProtocol.ParseResponse(ms));
 
             Assert.Equal(expectedMessage, exception.InnerException.Message);
+        }
+
+        [Fact]
+        public void WriteNegotiateResponseWithNullAvailableTransports()
+        {
+            MemoryBufferWriter writer = new MemoryBufferWriter();
+
+            NegotiateProtocol.WriteResponse(new NegotiationResponse(), writer);
+
+            string json = Encoding.UTF8.GetString(writer.ToArray());
+
+            Assert.Equal("{\"availableTransports\":[]}", json);
+        }
+
+        [Fact]
+        public void WriteNegotiateResponseWithNullTransferFormats()
+        {
+            MemoryBufferWriter writer = new MemoryBufferWriter();
+
+            NegotiateProtocol.WriteResponse(new NegotiationResponse
+            {
+                AvailableTransports = new List<AvailableTransport>
+                {
+                    new AvailableTransport()
+                }
+            }, writer);
+
+            string json = Encoding.UTF8.GetString(writer.ToArray());
+
+            Assert.Equal("{\"availableTransports\":[{\"transport\":null,\"transferFormats\":[]}]}", json);
         }
     }
 }
