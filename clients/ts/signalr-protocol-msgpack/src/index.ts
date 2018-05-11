8349f3b90db030b0331c8f95013dd43af1cede13
Andrew Stanton-Nurse <andrew@stanton-nurse.com>
2018-05-11 12:10:02 -0700

    Fix comment on signalr-protocol-msgpack

--- clients/ts/signalr-protocol-msgpack/src/index.ts
+++ clients/ts/signalr-protocol-msgpack/src/index.ts
@@ -2,7 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 // Version token that will be replaced by the prepack command
-/** The version of the SignalR client. */
+/** The version of the SignalR Message Pack protocol library. */
 export const VERSION = "0.0.0-DEV_BUILD";
 
 export { MessagePackHubProtocol } from "./MessagePackHubProtocol";
