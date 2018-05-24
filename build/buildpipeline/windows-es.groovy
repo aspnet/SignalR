29715c0b1cab9d2fa3f8c75f5373286793cf5e5e
Pranav K <prkrishn@hotmail.com>
2018-05-24 15:49:24 -0700

    Use correct machine affinity for Spanish language image

--- build/buildpipeline/windows-es.groovy
+++ build/buildpipeline/windows-es.groovy
@@ -2,7 +2,7 @@
 
 // 'node' indicates to Jenkins that the enclosed block runs on a node that matches
 // the label 'windows-with-vs'
-simpleNode('Windows.10.Amd64.ClientRS4.ES') {
+simpleNode('Windows.10.Amd64.ClientRS4.ES.Open') {
     stage ('Checking out source') {
         checkout scm
     }
