56c2e9aee45289d98a86945dd8f8a740a0910009
DylanDmitri <d.dylan.g@gmail.com>
2018-06-06 13:21:12 -0700

    example program

---
+++ samples/ClientSample/UploadSample.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.AspNetCore.SignalR.Protocol;
+using Microsoft.Extensions.CommandLineUtils;
+using System;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace ClientSample
+{
+    class UploadSample
+    {
+        internal static void Register(CommandLineApplication app)
+        {
+            app.Command("uploading", cmd =>
+            {
+                cmd.Description = "Tests a streaming from client to hub";
+
+                var baseUrlArgument = cmd.Argument("<BASEURL>", "The URL to the Chat Hub to test");
+
+                cmd.OnExecute(() => ExecuteAsync(baseUrlArgument.Value));
+            });
+        }
+        public static async Task<int> ExecuteAsync(string baseUrl)
+        {
+            var connection = new HubConnectionBuilder()
+                .WithUrl(baseUrl)
+                .Build();
+
+            await connection.StartAsync();
+
+            var channel = Channel.CreateUnbounded<char>();
+            var uploadStream = connection.InvokeAsync<string>("UploadWord", channel.Reader);
+
+            foreach (char c in "hello world")
+            {
+                await channel.Writer.WriteAsync(c);
+                await Task.Delay(300);
+            }
+
+            channel.Writer.TryComplete();
+
+            var result = await uploadStream;
+            // check if it's errored
+
+            return 0;
+        }
+
+        public static async Task<int> ExecuteAsyncTwo(string baseUrl)
+        {
+            var connection = new HubConnectionBuilder()
+                .WithUrl(baseUrl)
+                .Build();
+
+            await connection.StartAsync();
+
+            var channel = Channel.CreateUnbounded<char>();
+            _ = WriteLettersAsync(channel.Writer);
+            var result = await connection.InvokeAsync<string>("UploadWord", channel.Reader);
+
+            // check if it's errored
+
+            return 0;
+
+            async Task WriteLettersAsync(ChannelWriter<char> writer)
+            {
+                foreach (char c in "hello world")
+                {
+                    await Task.Delay(1000);
+                    await writer.WriteAsync(c);
+                }
+                writer.TryComplete(); // should send StreamCompleteMessage
+            }
+        }
+
+
+        //public static async Task<int> ExecuteAsyncThree(string baseUrl)
+        //{
+        //    var connection = new HubConnectionBuilder().WithUrl(baseUrl).Build();
+
+        //    await connection.StartAsync();
+
+        //    var result = await connection.StreamToServerAsync<string>(
+        //        source: WriteLettersAsync,
+        //        channel: Channel.CreateUnbounded<char>(),
+        //        target: "UploadWord"
+        //    );
+
+        //    return 0;
+
+        //    async Task WriteLettersAsync(ChannelWriter<char> writer)
+        //    {
+        //        foreach (char c in "hello world")
+        //        {
+        //            await Task.Delay(1000);
+        //            await writer.WriteAsync(c);
+        //        }
+        //    }
+        //}
+    }
+}
---
+++ samples/SignalRSamples/Hubs/UploadHub.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Microsoft.AspNetCore.SignalR.Protocol;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace SignalRSamples.Hubs
+{
+    public class UploadHub : Hub
+    {
+        public async Task<string> UploadWord(ChannelReader<char> source)
+        {
+            var sb = new StringBuilder();
+
+            // receiving a StreamCompleteMessage should cause this WaitToRead to return false
+            while (await source.WaitToReadAsync())
+            {
+                while (source.TryRead(out var item))
+                {
+                    Console.WriteLine($"received: {item}");
+                    sb.Append(item);
+                }
+            }
+
+            // method returns, somewhere else returns a CompletionMessage with any errors
+            return sb.ToString();
+        }
+    }
+}
