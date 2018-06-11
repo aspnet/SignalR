f05c3494f36fe07ff6c8e5c8a2248ed0a34b2e89
BrennanConroy <brecon@microsoft.com>
2018-06-11 12:41:29 -0700

    Try OSX

--- build/buildpipeline/osx.groovy
+++ build/buildpipeline/osx.groovy
@@ -1,2 +1,14 @@
 @Library('dotnet-ci') _
 
+// 'node' indicates to Jenkins that the enclosed block runs on a node that matches
+// the label 'windows-with-vs'
+simpleNode('OSX10.13.Amd64.Open') {
+    stage ('Checking out source') {
+        checkout scm
+    }
+    stage ('Build') {
+        def logFolder = getLogFolder()
+        def environment = "export ASPNETCORE_TEST_LOG_DIR=${WORKSPACE}\\${logFolder}"
+        bat "${environment}&.\\run.sh --ci default-build"
+    }
+}
