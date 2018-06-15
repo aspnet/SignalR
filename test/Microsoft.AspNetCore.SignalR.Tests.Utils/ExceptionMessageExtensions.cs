5eec568f6000c31f13d023be89540c7325597687
Andrew Stanton-Nurse <andrew@stanton-nurse.com>
2018-06-15 13:57:34 -0700

    derp

--- test/Microsoft.AspNetCore.SignalR.Tests.Utils/ExceptionMessageExtensions.cs
+++ test/Microsoft.AspNetCore.SignalR.Tests.Utils/ExceptionMessageExtensions.cs
@@ -9,7 +9,7 @@ namespace Microsoft.AspNetCore.SignalR.Tests
             // Strip off the last line since it's "Parameter Name: [parameterName]" and:
             // 1. We verify the parameter name separately
             // 2. It is localized, so we don't want our tests to break in non-US environments
-            var message = argex.Message;
+            var message = argEx.Message;
             var lastNewline = message.LastIndexOf(Environment.NewLine, StringComparison.Ordinal);
             if (lastNewline < 0)
             {
