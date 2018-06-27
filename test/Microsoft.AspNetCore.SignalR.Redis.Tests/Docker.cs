5b75bdc42bcb8f0bb1849177210573225c65cb94
BrennanConroy <brecon@microsoft.com>
2018-06-27 15:19:36 -0700

    cleanup

--- test/Microsoft.AspNetCore.SignalR.Redis.Tests/Docker.cs
+++ test/Microsoft.AspNetCore.SignalR.Redis.Tests/Docker.cs
@@ -36,17 +36,13 @@ namespace Microsoft.AspNetCore.SignalR.Redis.Tests
             }
 
             var docker = new Docker(location);
-            Console.WriteLine($"docker locations: {location}");
 
-            // Windows docker must have Linux containers turned on, if they don't skip the docker tests
-            //if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            docker.RunCommand("info --format '{{.OSType}}'", out var output);
+
+            if (!string.Equals(output.Trim('\'', '"', '\r', '\n', ' '), "linux"))
             {
-                docker.RunCommand("info --format '{{.OSType}}'", out var output);
-                Console.WriteLine(output);
-                if (!string.Equals(output.Trim('\'', '"', '\r', '\n', ' '), "linux"))
-                {
-                    return null;
-                }
+                Console.WriteLine($"'docker info' output: {output}");
+                return null;
             }
 
             return docker;
