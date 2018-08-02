801f5070de70af64f26fc14283fe4ffc356c8d7b
unknown <mimengis@microsoft.com>
2018-08-02 10:02:29 -0700

    cleanup

--- clients/java/signalr/src/main/java/LogLevel.java
+++ clients/java/signalr/src/main/java/LogLevel.java
@@ -12,24 +12,3 @@ public enum LogLevel {
     public int value;
     LogLevel(int id) { this.value = id; }
 }
-
-/*
-*
-*export enum LogLevel {
-    /** Log level for very low severity diagnostic messages.
-    Trace = 0,
-            /** Log level for low severity diagnostic messages.
-            Debug = 1,
-            /** Log level for informational diagnostic messages.
-            Information = 2,
-            /** Log level for diagnostic messages that indicate a non-fatal problem.
-            Warning = 3,
-            /** Log level for diagnostic messages that indicate a failure in the current operation.
-            Error = 4,
-            /** Log level for diagnostic messages that indicate a failure that will terminate the entire application.
-            Critical = 5,
-            /** The highest possible log level. Used when configuring logging to indicate that no log messages should be emitted.
-            None = 6,
-            }
-*
-* */
