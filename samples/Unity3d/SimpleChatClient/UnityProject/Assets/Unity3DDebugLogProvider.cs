a8b895c3e139a4527bb9722cff264029f03c6481
Hannes Nel <hannesne@users.noreply.github.com>
2018-08-17 22:40:33 +1200

    Added unity client project.

---
+++ samples/Unity3d/SimpleChatClient/UnityProject/Assets/Unity3DDebugLogProvider.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Logging;
+
+public class Unity3DDebugLogProvider : ILoggerProvider
+{
+    public void Dispose()
+    {
+        
+    }
+
+    public ILogger CreateLogger(string categoryName)
+    {
+        return new Unity3DDebugLog(categoryName);
+    }
+}
