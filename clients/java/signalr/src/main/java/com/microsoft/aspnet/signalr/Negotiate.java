27812b3775e42523183de2f97090f5be9ce45be5
MikaelMengistu <ethmengistu7@gmail.com>
2018-08-29 23:59:38 -0700

    clean up

--- clients/java/signalr/src/main/java/com/microsoft/aspnet/signalr/Negotiate.java
+++ clients/java/signalr/src/main/java/com/microsoft/aspnet/signalr/Negotiate.java
@@ -7,53 +7,32 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
-import org.apache.http.HttpResponse;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URISyntaxException;
 
 public class Negotiate {
 
     public static NegotiateResponse processNegotiate(String url) throws IOException {
+        return processNegotiate(url, null);
+    }
+
+    public static NegotiateResponse processNegotiate(String url, String accessTokenHeader) throws IOException {
         url = resolveNegotiateUrl(url);
         OkHttpClient client = new OkHttpClient();
         RequestBody body = RequestBody.create(null, new byte[]{});
-        Request request =  new Request.Builder()
+        Request.Builder requestBuilder =  new Request.Builder()
                 .url(url)
-                .post(body)
-                .build();
-        Response response = client.newCall(request).execute();
-        String result = response.body().string();
-        return new NegotiateResponse(result);
-    }
+                .post(body);
 
-    public static NegotiateResponse processNegotiate(String url, String accessTokenHeader) throws IOException, URISyntaxException {
-        url = resolveNegotiateUrl(url);
-        OkHttpClient client = new OkHttpClient();
-        RequestBody body = RequestBody.create(null, new byte[]{});
-        Request request =  new Request.Builder()
-                .url(url)
-                .addHeader("Authorization", "Bearer " + accessTokenHeader)
-                .post(body)
-                .build();
-        Response response = client.newCall(request).execute();
-        String result = response.body().string();
-        return new NegotiateResponse(result);
-    }
-
-    private static String getResponseResult(HttpResponse response) throws IOException {
-        BufferedReader rd = new BufferedReader(
-                new InputStreamReader(response.getEntity().getContent()));
-
-        StringBuffer result = new StringBuffer();
-        String line = "";
-        while ((line = rd.readLine()) != null) {
-            result.append(line);
+        if(accessTokenHeader != null) {
+            requestBuilder.addHeader("Authorization", "Bearer " + accessTokenHeader)
         }
 
-        return result.toString();
+        Request request = requestBuilder.build();
+
+        Response response = client.newCall(request).execute();
+        String result = response.body().string();
+        return new NegotiateResponse(result);
     }
 
     private static String resolveNegotiateUrl(String url){
