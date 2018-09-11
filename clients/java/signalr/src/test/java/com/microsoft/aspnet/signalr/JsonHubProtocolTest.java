72312fe50e1c147175ec74cefcfd1e90b2e0cb67
MikaelMengistu <ethmengistu7@gmail.com>
2018-09-11 11:40:36 -0700

    Make things package-private and move test classes to same package as source

--- clients/java/signalr/src/test/java/com/microsoft/aspnet/signalr/JsonHubProtocolTest.java
+++ clients/java/signalr/src/test/java/com/microsoft/aspnet/signalr/JsonHubProtocolTest.java
@@ -1,7 +1,7 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-package com.microsoft.aspnet.signalr.test;
+package com.microsoft.aspnet.signalr;
 
 import static org.junit.Assert.*;
 
