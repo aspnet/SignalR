0b2cf313502bc29ccb4d50db50ca1cd213eba63e
MikaelMengistu <ethmengistu7@gmail.com>
2018-09-20 12:01:04 -0700

    Spotless

--- clients/java/signalr/src/test/java/com/microsoft/aspnet/signalr/HttpConnectionOptionsTest.java
+++ clients/java/signalr/src/test/java/com/microsoft/aspnet/signalr/HttpConnectionOptionsTest.java
@@ -7,9 +7,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.jupiter.api.Test;
-
 import java.util.concurrent.CompletableFuture;
+
+import org.junit.jupiter.api.Test;
 
 public class HttpConnectionOptionsTest {
     @Test
