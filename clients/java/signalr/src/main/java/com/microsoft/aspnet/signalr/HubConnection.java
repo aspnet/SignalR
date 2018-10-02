be61d17d3223ee692c3dc26be61b1528e86171c9
MikaelMengistu <ethmengistu7@gmail.com>
2018-10-01 17:42:04 -0700

    spotless

--- clients/java/signalr/src/main/java/com/microsoft/aspnet/signalr/HubConnection.java
+++ clients/java/signalr/src/main/java/com/microsoft/aspnet/signalr/HubConnection.java
@@ -4,7 +4,6 @@
 package com.microsoft.aspnet.signalr;
 
 import java.io.IOException;
-import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
