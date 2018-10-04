b943099b844ff92fe43cffe9e406dfc29c33ffa5
MikaelMengistu <ethmengistu7@gmail.com>
2018-10-04 16:18:59 -0700

    Wait for handshake

--- clients/java/signalr/src/main/java/com/microsoft/signalr/HttpHubConnectionBuilder.java
+++ clients/java/signalr/src/main/java/com/microsoft/signalr/HttpHubConnectionBuilder.java
@@ -3,6 +3,7 @@
 
 package com.microsoft.signalr;
 
+import java.time.Duration;
 import java.util.concurrent.CompletableFuture;
 import java.util.function.Supplier;
 
@@ -15,6 +16,7 @@ public class HttpHubConnectionBuilder {
     private HttpClient httpClient;
     private boolean skipNegotiate;
     private Single<String> accessTokenProvider;
+    private Duration handshakeResponseTimeout;
 
     HttpHubConnectionBuilder(String url) {
         this.url = url;
@@ -56,7 +58,12 @@ public class HttpHubConnectionBuilder {
         return this;
     }
 
+    HttpHubConnectionBuilder withHandshakeResponseTimeout(Duration timeout) {
+        this.handshakeResponseTimeout = timeout;
+        return this;
+    }
+
     public HubConnection build() {
-        return new HubConnection(url, transport, skipNegotiate, logger, httpClient, accessTokenProvider);
+        return new HubConnection(url, transport, skipNegotiate, logger, httpClient, accessTokenProvider, handshakeResponseTimeout);
     }
 }
