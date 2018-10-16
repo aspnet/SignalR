68950a977a4340cb8d9751526436728d89827f1e
MikaelMengistu <ethmengistu7@gmail.com>
2018-10-16 12:02:41 -0700

    Package description

--- clients/java/signalr/src/main/java/com/microsoft/signalr/package-info.java
+++ clients/java/signalr/src/main/java/com/microsoft/signalr/package-info.java
@@ -1,4 +1,8 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+/**
+ * This package contains the classes for SignalR Java client.
+ * SignalR Java client.
+ */
 package com.microsoft.signalr;
