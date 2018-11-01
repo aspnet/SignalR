7eadc4e2731af81b71c7062a5db0f927321434bf
Andrew Stanton-Nurse <andrew@stanton-nurse.com>
2018-11-01 15:10:04 -0700

    Update clients/java/signalr/src/test/java/com/microsoft/signalr/WebSocketTransportTest.java
    Co-Authored-By: BrennanConroy <brecon@microsoft.com>

--- clients/java/signalr/src/test/java/com/microsoft/signalr/WebSocketTransportTest.java
+++ clients/java/signalr/src/test/java/com/microsoft/signalr/WebSocketTransportTest.java
@@ -17,7 +17,7 @@ import io.reactivex.Single;
 
 class WebSocketTransportTest {
     @Test
-    public void WebsocketThrowsIfItCantConnect() {
+    public void WebSocketThrowsIfItCantConnect() {
         Transport transport = new WebSocketTransport(new HashMap<>(), new DefaultHttpClient());
         RuntimeException exception = assertThrows(RuntimeException.class, () -> transport.start("http://url.fake").blockingAwait(1, TimeUnit.SECONDS));
         assertEquals("There was an error starting the Websockets transport.", exception.getMessage());
